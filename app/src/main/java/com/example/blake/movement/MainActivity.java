f4a226d239709641c954e5499edf33aa328c5440
Blake Hawkins <hawkinsba@hendrix.edu>
2019-01-30 10:09:13 -0600

    GUI updates in MainActivity.java
    Dialogue box with spinner now appears when the collect button is pressed, added toast

--- app/src/main/java/com/example/blake/movement/MainActivity.java
+++ app/src/main/java/com/example/blake/movement/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.blake.movement;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,17 +24,31 @@ public class MainActivity extends AppCompatActivity {
         collect = findViewById(R.id.collectButton);
         review = findViewById(R.id.reviewButton);
 
-        //Trying to populate the GPS settings spinner (which should contain "Default" and "Manual"
-        //https://developer.android.com/guide/topics/ui/controls/spinner#java
-        gpsSettings = findViewById(R.id.gpsSettings);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.settings_choices, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        gpsSettings.setAdapter(adapter);
-
+        review.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Review pressed", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         collect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Collect pressed", Toast.LENGTH_SHORT).show();
+
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.settings_dialogue, null);
+
+                //Trying to populate the GPS settings spinner (which should contain "Default" and "Manual"
+                //https://developer.android.com/guide/topics/ui/controls/spinner#java
+                gpsSettings = mView.findViewById(R.id.gpsSettings);
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.settings_choices, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                gpsSettings.setAdapter(adapter);
+
+                mBuilder.setView(mView);
+                AlertDialog settingsDialog = mBuilder.create();
+                settingsDialog.show();
 
             }
         });
