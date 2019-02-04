02fe3e57e87a6f3d132ce992edc58a4af011c42f
Blake <blake.hawkins97@gmail.com>
2019-02-04 12:13:41 -0600

    Update MainActivity.java
    Removed unnecessary comments

--- app/src/main/java/com/example/blake/movement/MainActivity.java
+++ app/src/main/java/com/example/blake/movement/MainActivity.java
@@ -27,9 +27,6 @@ public class MainActivity extends AppCompatActivity {
 
         collect = findViewById(R.id.collectButton);
         review = findViewById(R.id.reviewButton);
-        // Idea: Need to inflate settings_dialogue.xml and set toGPS through mView or else toGPS is a null-pointer
-        // 2/1/2019 10:33 AM: Something's not working - cannot access onClickListener for toGPS.
-        // 2/1/2019 10:52 AM: After testing with log messages, it doesn't look like toGPS is null after toGPS = mView.findViewById(R.id.settingsConfirm);
         View mView = getLayoutInflater().inflate(R.layout.settings_dialogue, null);
         toGPS = mView.findViewById(R.id.settingsConfirm);
         if (toGPS == null){
@@ -52,8 +49,6 @@ public class MainActivity extends AppCompatActivity {
                 AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
                 View mView = getLayoutInflater().inflate(R.layout.settings_dialogue, null);
 
-                // Trying to populate the GPS settings spinner (which should contain "Default" and "Manual"
-                // https://developer.android.com/guide/topics/ui/controls/spinner#java
                 gpsSettings = mView.findViewById(R.id.gpsSettings);
                 ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this, R.array.settings_choices, android.R.layout.simple_spinner_item);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
